7758ef1a0cf42fe4211c84732b37241a668ae47f
EthanJWright <ewright362@gmail.com>
2021-08-18 20:19:27 -0600

    feat: run command after sending to buffer

--- lua/harpoon/init.lua
+++ lua/harpoon/init.lua
@@ -156,6 +156,7 @@ M.setup = function(config)
         global_settings = {
             ["save_on_toggle"] = false,
             ["save_on_change"] = true,
+            ["enter_on_sendcmd"] = false,
         },
     }, expand_dir(
         c_config
