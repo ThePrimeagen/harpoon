323027715f2a2799534e363bb64b5c111bb0f62e
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-08-30 02:28:47 +0530

    harpoon dynamic commands for projects

--- lua/harpoon/term.lua
+++ lua/harpoon/term.lua
@@ -51,6 +51,17 @@ local function find_terminal(args)
     return term_handle
 end
 
+local function get_first_empty_slot()
+    log.trace("_get_first_empty_slot()")
+    for idx, cmd in pairs(harpoon.get_term_config().cmds) do
+        if cmd == "" then
+            return idx
+        end
+    end
+    return M.get_length() + 1;
+end
+
+
 M.gotoTerminal = function(idx)
     log.trace("gotoTerminal(): Terminal:", idx)
     local term_handle = find_terminal(idx)
@@ -78,6 +89,53 @@ M.clear_all = function()
         vim.api.nvim_buf_delete(term.buf_id, { force = true })
     end
     terminals = {}
+end
+
+M.get_length = function()
+    log.trace("_get_length()")
+    return table.maxn(harpoon.get_term_config().cmds)
+end
+
+M.valid_index = function(idx)
+    if idx == nil or idx > M.get_length() or idx <= 0 then
+        return false
+    end
+    return true
+end
+
+M.emit_changed = function()
+    log.trace("_emit_changed()")
+    if harpoon.get_global_settings().save_on_change then
+        harpoon.save()
+    end
+end
+
+M.add_cmd = function(cmd)
+    log.trace("add_cmd()")
+    local found_idx = get_first_empty_slot();
+    harpoon.get_term_config().cmds[found_idx] = cmd
+    M.emit_changed()
+end
+
+M.rm_cmd = function(idx)
+    log.trace("rm_cmd()")
+    if not M.valid_index(idx) then
+        log.debug("rm_cmd(): no cmd exists for index", idx)
+        return
+    end
+    table.remove(harpoon.get_term_config().cmds, idx)
+    M.emit_changed()
+end
+
+M.set_cmd_list = function(new_list)
+  log.trace("set_cmd_list(): New list:", new_list)
+  for k in pairs(harpoon.get_term_config().cmds) do
+    harpoon.get_term_config().cmds[k] = nil
+  end
+  for k, v in pairs(new_list) do
+    harpoon.get_term_config().cmds[k] = v
+  end
+  M.emit_changed()
 end
 
 return M
