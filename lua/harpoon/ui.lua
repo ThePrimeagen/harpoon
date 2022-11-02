963677d4c3ca348b99c3b387480308354553b7ac
Marcel Beumer <marcel@marcelbeumer.com>
2022-11-02 18:01:07 +0100

    fix: do not force absolute file paths
    When forcing absolute filepaths buffer names will get that absolute
    filepath too. To me it seems that vim.fn.bufadd supports relative paths
    just fine.

--- lua/harpoon/ui.lua
+++ lua/harpoon/ui.lua
@@ -173,11 +173,7 @@ function M.nav_file(id)
     end
 
     local mark = Marked.get_marked_file(idx)
-    local filename = mark.filename
-    if filename:sub(1, 1) ~= "/" then
-        filename = vim.loop.cwd() .. "/" .. mark.filename
-    end
-    local buf_id = get_or_create_buffer(filename)
+    local buf_id = get_or_create_buffer(mark.filename)
     local set_row = not vim.api.nvim_buf_is_loaded(buf_id)
 
     vim.api.nvim_set_current_buf(buf_id)
