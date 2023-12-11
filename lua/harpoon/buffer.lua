602947ec0309f3c58ea628cdfa14023a4ea6e661
tris203 <admin@snappeh.com>
2023-12-11 00:09:28 +0000

    fix: restore acwrite

--- lua/harpoon/buffer.lua
+++ lua/harpoon/buffer.lua
@@ -45,7 +45,8 @@ function M.setup_autocmds_and_keymaps(bufnr)
         vim.api.nvim_buf_set_name(bufnr, get_harpoon_menu_name())
     end
 
-    vim.api.nvim_buf_set_option(bufnr, "filetype", "harpoon")
+    vim.api.nvim_set_option_value("filetype", "harpoon", { buf = bufnr })
+    vim.api.nvim_set_option_value("buftype", "acwrite", { buf = bufnr })
     vim.keymap.set("n", "q", function()
         M.run_toggle_command("q")
     end, { buffer = bufnr, silent = true })
