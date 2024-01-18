4bc01995b16c6c72f7a9d08c5833162ed52091c5
tusqasi <2002.tushar.kuntawar@gmail.com>
2024-01-19 00:11:58 +0530

    Cross platform paths

--- lua/harpoon/data.lua
+++ lua/harpoon/data.lua
@@ -1,7 +1,8 @@
 local Path = require("plenary.path")
 
 local data_path = vim.fn.stdpath("data")
-local full_data_path = string.format("%s/harpoon.json", data_path)
+local full_data_path = string.format("%s%s%s",data_path, Path.sep, "harpoon.json")
+-- local full_data_path = string.format("%s/harpoon.json", data_path)
 
 ---@param data any
 local function write_data(data)
