d0805bd877a4948fe495fbf550e81d1217f4cc47
Kim A. Brandt <kim.brandt@gsgroup.dk>
2024-01-20 19:58:44 +0100

    fix(config): prevent the `Vim:E325: ATTENTION`-error
    This change seems to get rid of the error (not the message), that
    happens when loading a buffer with a swap-file; when the
    `swapfile`-option is enabled. The error interrupted the code, so that
    the buffer was not loaded, on the first `select()`.

--- lua/harpoon/config.lua
+++ lua/harpoon/config.lua
@@ -108,6 +108,7 @@ function M.get_default_config()
                     bufnr = vim.fn.bufnr(list_item.value, true)
                 end
                 if not vim.api.nvim_buf_is_loaded(bufnr) then
+                    vim.api.nvim_set_current_buf(bufnr)
                     vim.fn.bufload(bufnr)
                     vim.api.nvim_set_option_value("buflisted", true, {
                         buf = bufnr,
