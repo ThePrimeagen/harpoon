e9e5f147cbbc25ac8e7cc99512f334edc2975193
Kim A. Brandt <kim.brandt@gsgroup.dk>
2024-01-23 08:10:02 +0100

    On select, always let the buffer show in the buffer list, even when `bufload` encounters an error.

--- lua/harpoon/config.lua
+++ lua/harpoon/config.lua
@@ -108,12 +108,10 @@ function M.get_default_config()
                     bufnr = vim.fn.bufnr(list_item.value, true)
                 end
                 if not vim.api.nvim_buf_is_loaded(bufnr) then
-                    local ok = pcall(vim.fn.bufload, bufnr)
-                    if ok then
-                        vim.api.nvim_set_option_value("buflisted", true, {
-                            buf = bufnr,
-                        })
-                    end
+                    pcall(vim.fn.bufload, bufnr)
+                    vim.api.nvim_set_option_value("buflisted", true, {
+                        buf = bufnr,
+                    })
                 end
 
                 if options.vsplit then
